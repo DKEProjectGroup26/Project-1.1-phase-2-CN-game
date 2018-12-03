54039a27c29c0ee9e932d4c69daa8526f168bfda
gorilskij <43991400@users.noreply.github.com>
2018-12-03 22:09:55 +0100

    Update Play.java

--- game/game_mode3/Play.java
+++ game/game_mode3/Play.java
@@ -6,7 +6,6 @@ import game.graph.GraphData;
 
 public class Play {
     public static void start(GraphData data, WindowManager manager) {
-        
         var game = new Game("Random Order", 5, manager, data);
         
         game.standardSetup();
