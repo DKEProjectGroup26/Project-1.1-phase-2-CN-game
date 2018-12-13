47a9d336943c5341fb45267a01da27f665cef2ef
stephanpossen <43991595+stephanpossen@users.noreply.github.com>
2018-12-13 18:19:53 +0100

    Update Select.java

--- game/game_mode3/Select.java
+++ game/game_mode3/Select.java
@@ -14,7 +14,7 @@ public class Select {
         var menu = new Selection("Random Order", manager);
         
         menu.addText("Here your ability to think ahead will be tested. The way this game mode works is that the order of coloring the vertices will be decided for you . This means you will be forced to color the vertices in a specific way. Fortunately there is no need to complete the graph perfectly, but as always you should try to use the least amount of colors. Should you get stuck and run out of colors, then there is always an option to create a new color. Good luck!", 50);
-        
+        // again, the description of the gamerules
         menu.addSep();
         
         menu.addLabel("Please choose an option");
@@ -24,13 +24,13 @@ public class Select {
         menu.addButton("Generate a random graph", new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 var data = Generator.makeGraph();
-                Play.start(data, manager);
+                Play.start(data, manager); //again, creates a graph with random input and a timer will start at creation
             }
         });
         
         menu.addButton("Generate a random graph from parameters...", new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                Parameters.start(manager);
+                Parameters.start(manager); //again, switches to the menu where the player can choose #vertices, #edges
             }
         });
         
@@ -41,7 +41,7 @@ public class Select {
                 menu.visible();
                 if (path != null) {
                     var data = Reader.readGraph(path);
-                    Play.start(data, manager);
+                    Play.start(data, manager); //works the same way as in the other gamemodes
                 }
             }
         });
