7e61cfea88effc627fe4b79fd0d410a567e2dbd8
stephanpossen <43991595+stephanpossen@users.noreply.github.com>
2018-12-13 18:15:59 +0100

    Update Select.java

--- game/game_mode2/Select.java
+++ game/game_mode2/Select.java
@@ -13,7 +13,7 @@ public class Select {
         var menu = new Selection("Best in Time", manager);
         
         menu.addText("This game mode is for the stress-resistant people that are in our midst. The following rules apply here. You will be given an amount of time of your choosing to complete the graph. Here it is not necessary to do it perfectly, but of course you should try to do so. A clock will be counting down when the graph is created, ending the game when it reaches zero. Good luck!", 50);
-        
+        //description of the gamerules
         menu.addSep();
         
         menu.addLabel("Please choose an option");
@@ -23,13 +23,13 @@ public class Select {
         menu.addButton("Generate a random graph", new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 var data = Generator.makeGraph();
-                Play.start(data, 2, manager);
+                Play.start(data, 2, manager); //same as gamemode 1, creates a graph with random input, the amount of time is 30 seconds per vertex generated
             }
         });
         
         menu.addButton("Generate a random graph from parameters...", new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                Parameters.start(manager);
+                Parameters.start(manager); //creates a graph with user input, this time with a timer that is constantly visible
             }
         });
         
@@ -40,7 +40,7 @@ public class Select {
                 menu.visible();
                 if (path != null) {
                     var data = Reader.readGraph(path);
-                    Play.start(data, Math.max(1, data.nodes.length / 2), manager);
+                    Play.start(data, Math.max(1, data.nodes.length / 2), manager); //works the same as in gamemode 1, also the amount of time is 30 seconds per vertex generated
                 }
             }
         });
